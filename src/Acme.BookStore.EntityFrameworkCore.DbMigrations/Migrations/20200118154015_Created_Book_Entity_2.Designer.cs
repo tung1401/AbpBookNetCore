8a4bc24e57c48dc4afa5eea1d5e413b7d68d2c14
Patompol <patompol.taesuji@pranworks.com>
2020-01-19 22:23:25 +0700

    update

--- src/Acme.BookStore.EntityFrameworkCore.DbMigrations/Migrations/20200118154015_Created_Book_Entity_2.Designer.cs
+++ src/Acme.BookStore.EntityFrameworkCore.DbMigrations/Migrations/20200118154015_Created_Book_Entity_2.Designer.cs
@@ -4,14 +4,16 @@ using Acme.BookStore.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Acme.BookStore.Migrations
 {
     [DbContext(typeof(BookStoreMigrationsDbContext))]
-    partial class BookStoreMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200118154015_Created_Book_Entity_2")]
+    partial class Created_Book_Entity_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
